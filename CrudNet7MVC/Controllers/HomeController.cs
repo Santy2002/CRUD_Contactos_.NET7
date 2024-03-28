3811d3ca25727e494121c3ea30eeb4308fd3e462
L3SPA <santiagospairani@gmail.com>
2024-03-28 10:33:37 -0300

    Commit entire project

---
+++ CrudNet7MVC/Controllers/HomeController.cs
@@ -0,0 +1,133 @@
+﻿using CrudNet7MVC.Datos;
+using CrudNet7MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace CrudNet7MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Contactos.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Contactos.Add(contacto);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _context.Contactos.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(Contacto contacto)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Contactos.Update(contacto);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Detalles(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _context.Contactos.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpGet]
+        public IActionResult Borrar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _context.Contactos.Find(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpPost, ActionName("Borrar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> BorrarContacto(int? id)
+        {
+            var contacto = await _context.Contactos.FindAsync(id);
+            if (id == null)
+            {
+                return View();
+            }
+
+            _context.Contactos.Remove(contacto);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CrudNet7MVC/Models/Contacto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudNet7MVC.Models
+{
+    [NotMapped]
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo NOMBRE es OBLIGATORIO")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo CELULAR es OBLIGATORIO")]
+
+        public string Celular { get; set; }
+
+        [Required(ErrorMessage = "El campo EMAIL es OBLIGATORIO")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "El campo Email debe contener @")]
+        public string Email { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+
+    }
+}
